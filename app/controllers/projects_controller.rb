4318f83df029d612eb2503b4f5cf9c336fbd20cd
thiagofportella <thiagofportella@gmail.com>
2022-02-25 14:51:21 -0300

    feat: basic endpoints

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,19 +1,42 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[show destroy]
 
   def index
-    paginate json: current_user.projects
+    return head :no_content if current_user.projects.empty?
+
+    paginate json: current_user.projects.order(created_at: :desc), status: :ok
+  end
+
+  def show
+    if @project
+      render json: @project, status: :ok
+    else
+      head :no_content
+    end
+  end
+
+  def destroy
+    if @project.destroy
+      render json: ["Project successfully deleted"], status: :ok
+    else
+      render json: @project.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def create
     @project = current_user.projects.new(project_attributes)
     if @project.save
-      render json: { data: { project: @project.as_json } }, status: :created
+      render json: @project, status: :created
     else
-      render json: { data: { errors: @project.errors.full_messages } }, status: :unprocessable_entity
+      render json: @project.errors.full_messages, status: :unprocessable_entity
     end
   end
 
   private
+
+  def set_project
+    @project = current_user.projects.find_by_id(params[:id])
+  end
 
   def project_attributes
     params.require(:project).permit(:name)
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,34 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: :show
 
   def index
-    paginate json: current_user.tasks
+    return head :no_content if current_user.tasks.empty?
+
+    paginate json: current_user.tasks, status: :ok
+  end
+
+  def show
+    if @task
+      render json: @task, status: :ok
+    else
+      head :no_content
+    end
   end
 
   def create
     @task = current_user.tasks.new(task_params)
     if @task.save
-      render json: { data: { task: @task.as_json } }, status: :created
+      render json: @task, status: :created
     else
-      render json: { data: { errors: @task.errors.full_messages } }, status: :unprocessable_entity
+      render json: @task.errors.full_messages, status: :unprocessable_entity
     end
   end
 
-  private 
+  private
+
+  def set_task
+    @task = Task.find_by_id(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:name, :description, :sprint_id)

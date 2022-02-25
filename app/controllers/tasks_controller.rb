73646f84e5a00d434198ba985019f6f648c73c12
Felipe <felippe.felix98@gmail.com>
2022-02-25 12:37:03 -0300

    feat: adicionando action show de tarefas

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -38,6 +38,13 @@ class TasksController < ApplicationController
   def set_task
     @task = Task.find_by_id(params[:id])
   end
+  
+  def show
+    @task = Task.find(params[:id])
+    render json: { data: { task: @task } }, include: [:sprint, { votes: { include: :user } }]
+  end
+
+  private
 
   def task_params
     params.require(:task).permit(:name, :description, :sprint_id, :status_votation, :start_votation_time, :finish_votation_time, :limit_votation_time)

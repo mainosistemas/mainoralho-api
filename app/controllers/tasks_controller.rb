0a074a20abcbd5d20cf9fbb54622109e0d9b200b
SarahElesbao <sarahelesbao12@gmail.com>
2022-02-25 15:20:32 -0300

    refactor: testando as novas funcionalidades

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: :show
+  before_action :set_task, only: %i[show update]
 
   def index
     return head :no_content if current_user.tasks.empty?
@@ -9,7 +9,7 @@ class TasksController < ApplicationController
 
   def show
     if @task
-      render json: @task, status: :ok
+      render json: @task, include: [:sprint, { votes: { include: :user } }], status: :ok
     else
       head :no_content
     end
@@ -24,33 +24,22 @@ class TasksController < ApplicationController
       render json: @task.errors.full_messages, status: :unprocessable_entity
     end
   end
+ 
+  def update
+    if @task.update(task_params)
+      render json: { data: { task: @task.as_json } }, status: :ok
+    else
+      render json: { data: { errors: @task.errors.full_messages } }, status: :unprocessable_entity
+    end
+  end
 
   private
 
   def set_task
     @task = Task.find_by_id(params[:id])
   end
-  
-  def update
-    if @task.update(task_params)
-      render json: { data: { task: @task.as_json } }, status: :sucess
-    else
-      render json: { data: { errors: @task.errors.full_messages } }, status: :unprocessable_entity
-    end
-  end
-
-  def show
-    @task = Task.find(params[:id])
-    render json: { data: { task: @task } }, include: [:sprint, { votes: { include: :user } }]
-  end
-
-  private
 
   def task_params
     params.require(:task).permit(:name, :description, :sprint_id, :status_votation, :start_votation_time, :finish_votation_time, :limit_votation_time)
-  end
-
-  def set_task() 
-    @task = Task.find(params[:id])
   end
 end

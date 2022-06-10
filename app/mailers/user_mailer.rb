d4a00aea8df4de3db78ae4813a2aa727eac608ee
SarahElesbao <sarahelesbao12@gmail.com>
2022-06-10 11:38:08 -0300

    adicionando o env e o email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,11 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, subject: 'Bem-vindo(a) ao MAINORALHO!')
+  end
+
+  def recovery_password_email
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Recuperação de senha MAINORALHO' )
   end
 end

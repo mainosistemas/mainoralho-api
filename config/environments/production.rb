f1578252b7233147583da529486951b03091a7fe
SarahElesbao <sarahelesbao12@gmail.com>
2022-06-10 15:32:21 -0300

    alterações da senha

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = { api_token: Rails.application.credentials.postmark_api_token }
+
   host = 'mainoralho.herokuapp.com'
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 

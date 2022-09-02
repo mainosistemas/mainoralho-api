fee7d59d453a1edba2644295831a9069663a4c43
Alisson Araújo <alissonsdearaujo@gmail.com>
2022-09-02 11:46:36 -0300

    fix: notificando desconectado

--- app/channels/vote_channel.rb
+++ app/channels/vote_channel.rb
@@ -2,15 +2,26 @@ class VoteChannel < ApplicationCable::Channel
   def subscribed
     # puts "parametros: #{params}"
 
-    stream_from "sprint_channel_#{params[:room]}"
+    room = params[:room]
+
+    stream_from "sprint_channel_#{room}"
     puts "#{params}"
 
     user = User.find_by(email: params[:user][:email])
+    # armazena cada usuario conectado
+    # pegar usuarios da sessão
+    # notifica todos na sala
 
-    ActionCable.server.broadcast("sprint_channel_23", { message: user, type: 'conectado' })
+    ActionCable.server.broadcast("sprint_channel_#{room}", { message: user, type: 'conectado', users: @users })
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    puts "Desconectar usuário #{params}"
+
+    user = User.find_by(email: params[:user][:email])
+    room = params[:room]
+
+    ActionCable.server.broadcast("sprint_channel_#{room}", { message: user, type: 'desconectado' })
   end
 end
